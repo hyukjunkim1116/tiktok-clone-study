71b506a6ea0f9e915bed8a6ec0c2859145d62aa8
hyukjunkim1116 <nmkk1234@naver.com>
2024-04-08 18:23:11 +0900

    14.6 challenge

--- lib/users/user_profile_screen.dart
+++ lib/users/user_profile_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -24,8 +23,11 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    print(width);
     return SafeArea(
       child: DefaultTabController(
         length: 2,
@@ -47,143 +49,216 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               SliverToBoxAdapter(
                 child: Column(
                   children: [
-                    const CircleAvatar(
-                      radius: 50,
-                      foregroundImage: NetworkImage(
-                          "https://avatars.githubusercontent.com/u/3612017"),
-                      child: Text("니꼬"),
-                    ),
-                    Gaps.v20,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "@니꼬",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: Sizes.size18,
+                    if (width < 900) ...[
+                      const CircleAvatar(
+                        radius: 50,
+                        foregroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/3612017"),
+                        child: Text("니꼬"),
+                      ),
+                      Gaps.v20,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "@니꼬",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: Sizes.size18,
+                            ),
                           ),
+                          Gaps.h5,
+                          FaIcon(
+                            FontAwesomeIcons.solidCircleCheck,
+                            size: Sizes.size16,
+                            color: Colors.blue.shade500,
+                          )
+                        ],
+                      ),
+                      Gaps.v24,
+                      IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            UserProfileInfoText(
+                              info: '971',
+                              description: 'Following',
+                              infoSize: Sizes.size18,
+                              descriptionColor: Colors.grey.shade500,
+                            ),
+                            VerticalDivider(
+                              width: Sizes.size32,
+                              thickness: Sizes.size1,
+                              color: Colors.grey.shade400,
+                              indent: Sizes.size14,
+                              endIndent: Sizes.size14,
+                            ),
+                            UserProfileInfoText(
+                              info: "10M",
+                              description: "Followers",
+                              infoSize: Sizes.size18,
+                              descriptionColor: Colors.grey.shade500,
+                            ),
+                            VerticalDivider(
+                              width: Sizes.size32,
+                              thickness: Sizes.size1,
+                              color: Colors.grey.shade400,
+                              indent: Sizes.size14,
+                              endIndent: Sizes.size14,
+                            ),
+                            UserProfileInfoText(
+                              info: "194.3M",
+                              description: "Likes",
+                              infoSize: Sizes.size18,
+                              descriptionColor: Colors.grey.shade500,
+                            ),
+                          ],
                         ),
-                        Gaps.h5,
-                        FaIcon(
-                          FontAwesomeIcons.solidCircleCheck,
-                          size: Sizes.size16,
-                          color: Colors.blue.shade500,
-                        )
-                      ],
-                    ),
-                    Gaps.v24,
-                    IntrinsicHeight(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          UserProfileInfoText(
-                            info: '971',
-                            description: 'Following',
-                            infoSize: Sizes.size18,
-                            descriptionColor: Colors.grey.shade500,
-                          ),
-                          VerticalDivider(
-                            width: Sizes.size32,
-                            thickness: Sizes.size1,
-                            color: Colors.grey.shade400,
-                            indent: Sizes.size14,
-                            endIndent: Sizes.size14,
-                          ),
-                          UserProfileInfoText(
-                            info: "10M",
-                            description: "Followers",
-                            infoSize: Sizes.size18,
-                            descriptionColor: Colors.grey.shade500,
-                          ),
-                          VerticalDivider(
-                            width: Sizes.size32,
-                            thickness: Sizes.size1,
-                            color: Colors.grey.shade400,
-                            indent: Sizes.size14,
-                            endIndent: Sizes.size14,
-                          ),
-                          UserProfileInfoText(
-                            info: "194.3M",
-                            description: "Likes",
-                            infoSize: Sizes.size18,
-                            descriptionColor: Colors.grey.shade500,
-                          ),
-                        ],
+                      )
+                    ],
+                    if (width >= 900)
+                       Padding(
+                        padding:
+                            const EdgeInsets.symmetric(vertical: Sizes.size18),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 50,
+                                  foregroundImage: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/3612017"),
+                                  child: Text("니꼬"),
+                                ),
+                                Gaps.v10,
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "@니꼬",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: Sizes.size18,
+                                      ),
+                                    ),
+                                    Gaps.h5,
+                                    FaIcon(
+                                      FontAwesomeIcons.solidCircleCheck,
+                                      size: Sizes.size16,
+                                      color: Colors.blue,
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Gaps.h14,
+                            const IntrinsicHeight(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  UserProfileInfoText(
+                                    info: '971',
+                                    description: 'Following',
+                                    infoSize: Sizes.size18,
+                                    descriptionColor: Colors.grey,
+                                  ),
+                                  VerticalDivider(
+                                    width: Sizes.size32,
+                                    thickness: Sizes.size1,
+                                    color: Colors.grey,
+                                    indent: Sizes.size14,
+                                    endIndent: Sizes.size14,
+                                  ),
+                                  UserProfileInfoText(
+                                    info: "10M",
+                                    description: "Followers",
+                                    infoSize: Sizes.size18,
+                                    descriptionColor: Colors.grey,
+                                  ),
+                                  VerticalDivider(
+                                    width: Sizes.size32,
+                                    thickness: Sizes.size1,
+                                    color: Colors.grey,
+                                    indent: Sizes.size14,
+                                    endIndent: Sizes.size14,
+                                  ),
+                                  UserProfileInfoText(
+                                    info: "194.3M",
+                                    description: "Likes",
+                                    infoSize: Sizes.size18,
+                                    descriptionColor: Colors.grey,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Gaps.h20,
+                            SizedBox(
+                              height: 50,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size56,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(Sizes.size4),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Follow',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Gaps.h10,
+                            SizedBox.square(
+                              dimension: 50,
+                              child: Container(
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey.shade300,
+                                    width: 1,
+                                    style: BorderStyle.solid,
+                                  ),
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(Sizes.size4),
+                                  ),
+                                ),
+                                child: const FaIcon(
+                                  FontAwesomeIcons.youtube,
+                                ),
+                              ),
+                            ),
+                            Gaps.h10,
+                            SizedBox.square(
+                              dimension: 50,
+                              child: Container(
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey.shade300,
+                                    width: 1,
+                                    style: BorderStyle.solid,
+                                  ),
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(Sizes.size4),
+                                  ),
+                                ),
+                                child: const FaIcon(
+                                  FontAwesomeIcons.shapes,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Gaps.v14,
-                    FractionallySizedBox(
-                      widthFactor: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: 50,
-                            child: Container(
-                              alignment: Alignment.center,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: Sizes.size56,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(Sizes.size4),
-                                ),
-                              ),
-                              child: const Text(
-                                'Follow',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ),
-                          Gaps.h7,
-                          SizedBox.square(
-                            dimension: 50,
-                            child: Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.grey.shade300,
-                                  width: 1,
-                                  style: BorderStyle.solid,
-                                ),
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(Sizes.size4),
-                                ),
-                              ),
-                              child: const FaIcon(
-                                FontAwesomeIcons.youtube,
-                              ),
-                            ),
-                          ),
-                          Gaps.h7,
-                          SizedBox.square(
-                            dimension: 50,
-                            child: Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.grey.shade300,
-                                  width: 1,
-                                  style: BorderStyle.solid,
-                                ),
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(Sizes.size4),
-                                ),
-                              ),
-                              child: const FaIcon(
-                                FontAwesomeIcons.shapes,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
                     Gaps.v14,
                     const Padding(
                       padding: EdgeInsets.symmetric(
@@ -258,7 +333,9 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                               ),
                               Gaps.h6,
                               Text(
-                                index <10 ? "$index.${index}K" : "$index.${index%10}M",
+                                index < 10
+                                    ? "$index.${index}K"
+                                    : "$index.${index % 10}M",
                                 style: const TextStyle(
                                   color: Colors.white,
                                   fontSize: Sizes.size14,
