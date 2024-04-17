d95984ca597a031f73e5a0c45e86582a205e9bb0
hyukjunkim1116 <nmkk1234@naver.com>
2024-04-17 19:46:59 +0900

    23 firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
@@ -10,11 +11,13 @@ import 'package:tiktok_clone/router.dart';
 import 'package:tiktok_clone/videos/repos/playback_config_repo.dart';
 import 'package:tiktok_clone/videos/view_models/playback_config_vm.dart';
 
-import 'common/main_navigation/widgets/video_config.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
@@ -35,14 +38,14 @@ void main() async {
   );
 }
 
-class TikTokApp extends StatelessWidget {
+class TikTokApp extends ConsumerWidget {
   const TikTokApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
     // S.load(const Locale("en"));
     return MaterialApp.router(
-      routerConfig: router,
+      routerConfig: ref.watch(routerProvider),
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       localizationsDelegates: const [
--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/animation.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/videos/views/video_recording_screen.dart';
 import 'auth/login_screen.dart';
+import 'auth/repos/auth_repo.dart';
 import 'auth/sign_up_screen.dart';
 import 'common/main_navigation/main_navigation_screen.dart';
 import 'inbox/activity_screen.dart';
@@ -10,125 +11,84 @@ import 'inbox/chat_detail_screen.dart';
 import 'inbox/chats_screen.dart';
 import 'onboarding/interests_screen.dart';
 
-final router = GoRouter(
-  initialLocation: "/inbox",
-  routes: [
-    GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-    ),
-    GoRoute(
-      name: LoginScreen.routeName,
-      path: LoginScreen.routeURL,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      name: InterestsScreen.routeName,
-      path: InterestsScreen.routeURL,
-      builder: (context, state) => const InterestsScreen(),
-    ),
-    GoRoute(
-      path: "/:tab(home|discover|inbox|profile)",
-      name: MainNavigationScreen.routeName,
-      builder: (context, state) {
-        final tab = state.pathParameters["tab"]!;
-        return MainNavigationScreen(tab: tab);
-      },
-    ),
-    GoRoute(
-      name: ActivityScreen.routeName,
-      path: ActivityScreen.routeURL,
-      builder: (context, state) => const ActivityScreen(),
-    ),
-    GoRoute(
-      name: ChatsScreen.routeName,
-      path: ChatsScreen.routeURL,
-      builder: (context, state) => const ChatsScreen(),
-      routes: [
-        GoRoute(
-          name: ChatDetailScreen.routeName,
-          path: ChatDetailScreen.routeURL,
-          builder: (context, state) {
-            final chatId = state.pathParameters["chatId"]!;
-            return ChatDetailScreen(
-              chatId: chatId,
-            );
-          },
-        )
-      ],
-    ),
-    GoRoute(
-      path: VideoRecordingScreen.routeURL,
-      name: VideoRecordingScreen.routeName,
-      pageBuilder: (context, state) => CustomTransitionPage(
-        transitionDuration: const Duration(milliseconds: 200),
-        child: const VideoRecordingScreen(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          final position = Tween(
-            begin: const Offset(0, 1),
-            end: Offset.zero,
-          ).animate(animation);
-          return SlideTransition(
-            position: position,
-            child: child,
-          );
+
+final routerProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: "/home",
+    redirect: (context, state) {
+      final isLoggedIn = ref.read(authRepo).isLoggedIn;
+      if (!isLoggedIn) {
+        if (state.matchedLocation != SignUpScreen.routeURL &&
+            state.matchedLocation != LoginScreen.routeURL) {
+          return SignUpScreen.routeURL;
+        }
+      }
+      return null;
+    },
+    routes: [
+      GoRoute(
+        name: SignUpScreen.routeName,
+        path: SignUpScreen.routeURL,
+        builder: (context, state) => const SignUpScreen(),
+      ),
+      GoRoute(
+        name: LoginScreen.routeName,
+        path: LoginScreen.routeURL,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        name: InterestsScreen.routeName,
+        path: InterestsScreen.routeURL,
+        builder: (context, state) => const InterestsScreen(),
+      ),
+      GoRoute(
+        path: "/:tab(home|discover|inbox|profile)",
+        name: MainNavigationScreen.routeName,
+        builder: (context, state) {
+          final tab = state.pathParameters["tab"]!;
+          return MainNavigationScreen(tab: tab);
         },
       ),
-    ),
-  ],
-);
-
-//   name: SignUpScreen.routeName,
-//   path: SignUpScreen.routeURL,
-//   builder: (context, state) => const SignUpScreen(),
-//   routes: [
-//     GoRoute(
-//       path: UsernameScreen.routeURL,
-//       name: UsernameScreen.routeName,
-//       builder: (context, state) => const UsernameScreen(),
-//       routes: [
-//         GoRoute(
-//           name: EmailScreen.routeName,
-//           path: EmailScreen.routeURL,
-//           builder: (context, state) {
-//             final args = state.extra as EmailScreenArgs;
-//             return EmailScreen(username: args.username);
-//           },
-//         ),
-//       ],
-//     ),
-//   ],
-// ),
-/* GoRoute(
-      path: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
-    ), */
-/*  GoRoute(
-      name: "username_screen",
-      path: UsernameScreen.routeName,
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          child: const UsernameScreen(),
+      GoRoute(
+        name: ActivityScreen.routeName,
+        path: ActivityScreen.routeURL,
+        builder: (context, state) => const ActivityScreen(),
+      ),
+      GoRoute(
+        name: ChatsScreen.routeName,
+        path: ChatsScreen.routeURL,
+        builder: (context, state) => const ChatsScreen(),
+        routes: [
+          GoRoute(
+            name: ChatDetailScreen.routeName,
+            path: ChatDetailScreen.routeURL,
+            builder: (context, state) {
+              final chatId = state.pathParameters["chatId"]!;
+              return ChatDetailScreen(
+                chatId: chatId,
+              );
+            },
+          )
+        ],
+      ),
+      GoRoute(
+        path: VideoRecordingScreen.routeURL,
+        name: VideoRecordingScreen.routeName,
+        pageBuilder: (context, state) => CustomTransitionPage(
+          transitionDuration: const Duration(milliseconds: 200),
+          child: const VideoRecordingScreen(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: animation,
-              child: ScaleTransition(
-                scale: animation,
-                child: child,
-              ),
+            final position = Tween(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(animation);
+            return SlideTransition(
+              position: position,
+              child: child,
             );
           },
-        );
-      },
-    ), */
-//     GoRoute(
-//       path: "/users/:username",
-//       builder: (context, state) {
-//         final username = state.pathParameters['username'];
-//         final tab = state.uri.queryParameters["show"];
-//         return UserProfileScreen(username: username!, tab: tab!);
-//       },
-//     )
-//   ],
-// );
+        ),
+      )
+    ],
+  );
+});
